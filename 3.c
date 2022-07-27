3d13615b319e9282eb834f191de1d0a4b01ec5ec
karthik1946 <110052740+karthik1946@users.noreply.github.com>
2022-07-27 22:20:47 +0530

    calculated confined to four operations, cannot perform other operations

--- 3.c
+++ 3.c
@@ -22,3 +22,9 @@ int main() {
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
       break;
+   default:
+      printf("calculator performs only four operations");
+  }
+
+  return 0;
+}
